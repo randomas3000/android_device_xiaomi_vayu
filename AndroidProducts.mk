2b6841eda5a239a89d23dab782e58063704380df
vytautas <vytautas.duchas@gmail.com>
2022-05-06 00:14:52 +0300

    drop rom preFx

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/octavi_vayu.mk
+    $(LOCAL_DIR)/lineage_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    octavi_vayu-user \
-    octavi_vayu-userdebug \
-    octavi_vayu-eng
+    vayu-user \
+    vayu-userdebug \
+   vayu-eng
