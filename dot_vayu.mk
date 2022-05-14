863f0af4fa64223e31b7898c8e8880931c7e355d
vytautas <vytautas.duchas@gmail.com>
2022-05-14 15:18:31 +0300

    vayu: define bootanimation resolution

--- dot_vayu.mk
+++ dot_vayu.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/dot/config/common.mk)
 
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
